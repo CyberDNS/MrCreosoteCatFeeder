219b2e18f952c87b48872c83e68f9aef20978249
David Ney <david.ney@live.com>
2021-03-09 14:02:19 +0100

    untested migration to platform.io

---
+++ include/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+void setAction(byte action);
+
+void input();
+void process();
+void output();
+
+void inputAwaiting();
+void inputFeeding();
+
+void processIni();
+void processAwaiting();
+
+void outputFeed();
